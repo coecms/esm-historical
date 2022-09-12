18833eb4b2966e90ef3da0856451fff189e02bf9
Kieran Ricardo <kr4383@gadi-login-02.gadi.nci.org.au>
2022-09-12 16:00:02 +1000

    don't generate extra PBS jobs

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -20,4 +20,22 @@ arch_dir=/g/data/$PROJECT/$USER/archive/access-esm
 base_dir=/scratch/$PROJECT/$USER/access-esm/archive
 comp_proj=$PROJECT
 here=/g/data/tm70/kr4383/ACCESS-Archiver 
-/g/data/tm70/kr4383/ACCESS-Archiver/ACCESS_Archiver.sh
+
+plev8=false
+zonal=false
+convert_unknown=false
+ncexists=false
+arch_grp=$PROJECT
+UMDIR=/projects/access/umdir
+
+# setup dircetories
+mkdir -p $here/tmp/$loc_exp
+rm -f $here/tmp/$loc_exp/*
+mkdir -p $arch_dir/$loc_exp/{history/atm/netCDF,restart/atm}
+
+# run access archiving scripts
+${here}/subroutines/find_files_payu.sh # create file list
+python -W ignore $here/subroutines/run_um2nc.py # convert UM files to netcdf
+$here/subroutines/cp_hist.sh # copy over history
+$here/subroutines/cp_rest_payu.sh # copy over output files
+$here/subroutines/mppnccomb_check.sh # combine ocean output files
